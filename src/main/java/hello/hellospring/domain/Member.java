3be4d7b2d479510439a481ef8a17ac6c93646ade
DESKTOP-87RK9DC\jihyeon <osye1128@naver.com>
2021-04-26 18:12:30 +0900

    before db

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,22 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String name;//시스템이 저장하는 데이터
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
