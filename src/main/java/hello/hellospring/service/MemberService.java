351d1f176517f308ad7194732cd6ec0be4ba8956
DESKTOP-87RK9DC\jihyeon <osye1128@naver.com>
2021-04-28 17:26:04 +0900

    finally commit

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
 
@@ -38,7 +39,9 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+            return memberRepository.findAll();
+
     }
 
     public Optional<Member> findOne(Long memberId){
